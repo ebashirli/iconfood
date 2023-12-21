1d64b65bf4846db347f332c270a3d2c879b710fd
Elvin Bashirli <ebashirli@gmail.com>
2023-12-21 15:31:22 +0400

    fix pagination state

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
 import Button from "./Button";
 import styles from "./Pagination.module.scss";
 
 function Pagination() {
+  const [selected, setSelected] = useState(1);
   return (
     <div className={styles.container}>
-      <Button className={styles.first + " " + styles.button}>First</Button>
       <Button
-        className={styles.one + " " + styles.button + " " + styles.number}
+        className={`${styles.first} ${styles.button} ${
+          selected === 1 ? styles.notActive : ""
+        }`}
+        onClick={() => setSelected((s) => (s > 1 ? s - 1 : s))}
       >
-        1
-      </Button>
-      <Button
-        className={styles.two + " " + styles.button + " " + styles.number}
-      >
-        2
-      </Button>
-      <Button
-        className={styles.three + " " + styles.button + " " + styles.number}
-      >
-        3
+        First
       </Button>
 
-      <Button className={styles.mext + " " + styles.button}>Next</Button>
+      {[1, 2, 3].map((n) => (
+        <Button
+          key={n}
+          className={`${styles[`button-${n}`]} ${styles.button} ${
+            styles.numberBotton
+          } ${selected === n ? styles.selected : ""}`}
+          onClick={() => setSelected(n)}
+        >
+          {n}
+        </Button>
+      ))}
+
+      <Button
+        className={styles.mext + " " + styles.button}
+        onClick={() => setSelected((s) => (s < 3 ? s + 1 : s))}
+      >
+        Next
+      </Button>
     </div>
   );
 }
