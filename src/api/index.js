3db21dcb1ed0b092aec5bc92207ba50e338a9c71
Elvin Bashirli <ebashirli@gmail.com>
2023-12-23 16:35:51 +0400

    add remote server

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,5 @@
 import axios from "axios";
-
 export const instance = axios.create({
-  baseURL: "http://localhost:3001/ic-app/api/v1/",
-  headers: {
-    Authorization: "Bearer " + import.meta.env.VITE_API_TOKEN,
-  },
+  // baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://all-apps-dev.herokuapp.com/ic-app/api/v1/",
 });
